dede337423b758c12de0f4f9dd68dc36d6408c16
pit135 <wermasaubunpetrus@gmail.com>
2026-02-10 20:01:20 +0700

    functional test

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,20 +30,15 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-
-    testImplementation("org.seleniumhq.selenium:selenium-java:${seleniumJavaVersion}")
-    testImplementation("io.github.bonigarcia:selenium-jupiter:${seleniumJupiterVersion}")
-    testImplementation("io.github.bonigarcia:webdrivermanager:${webdrivermanagerVersion}")
-
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-
+    testImplementation("org.seleniumhq.selenium:selenium-java:4.14.1")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:5.0.1")
+    testImplementation("io.github.bonigarcia:webdrivermanager:5.6.3")
 
 }
 tasks.register<Test>("unitTest") {
