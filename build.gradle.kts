a1c8db7fd154daca7709a4c6f07fc876bb6cf274
pit135 <wermasaubunpetrus@gmail.com>
2026-02-10 13:51:09 +0700

    unit test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 val seleniumJavaVersion = "4.14.1"
 val seleniumJupiterVersion = "5.0.1"
 val webdrivermanagerVersion = "5.6.3"
-val junitJupiterVersion = "5.9.1"
 
 plugins {
     java
@@ -38,12 +37,14 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     testImplementation("org.seleniumhq.selenium:selenium-java:${seleniumJavaVersion}")
     testImplementation("io.github.bonigarcia:selenium-jupiter:${seleniumJupiterVersion}")
     testImplementation("io.github.bonigarcia:webdrivermanager:${webdrivermanagerVersion}")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitJupiterVersion}")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitJupiterVersion}")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+
 }
 tasks.register<Test>("unitTest") {
     description = "Runs unit tests."
